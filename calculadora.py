c6b925d0823ffe87412c6460f9008bf306474bb7
alejandro <camacho044@gmail.com>
2023-10-01 23:30:46 -0500

    primer commit del proyecto

---
+++ calculadora.py
@@ -0,0 +1,31 @@
+# Funciones para realizar operaciones matemáticas
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+# Programa principal
+if __name__ == "__main__":
+    print("Calculadora Simple")
+    while True:
+        print("\nOpciones:")
+        print("1. Suma")
+        print("2. Resta")
+        print("3. Salir")
+
+        opcion = input("Seleccione una opción (1/2/3): ")
+
+        if opcion == '3':
+            print("¡Hasta luego!")
+            break
+
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+
+        if opcion == '1':
+            print("Resultado: ", suma(num1, num2))
+        elif opcion == '2':
+            print("Resultado: ", resta(num1, num2))
+        else:
+            print("Opción no válida. Intente nuevamente.")
---
+++ main.py
@@ -0,0 +1,32 @@
+# Importar el módulo de la calculadora
+import calculadora
+
+# Programa principal
+if __name__ == "__main__":
+    while True:
+        print("\nCalculadora Simple")
+        print("Opciones:")
+        print("1. Realizar cálculos")
+        print("2. Salir")
+
+        opcion = input("Seleccione una opción (1/2): ")
+
+        if opcion == '2':
+            print("¡Hasta luego!")
+            break
+        elif opcion == '1':
+            print("\nOperaciones disponibles:")
+            print("1. Suma")
+            print("2. Resta")
+
+            operacion = input("Seleccione una operación (1/2/3/4): ")
+
+            num1 = float(input("Ingrese el primer número: "))
+            num2 = float(input("Ingrese el segundo número: "))
+
+            if operacion == '1':
+                print("Resultado: ", calculadora.suma(num1, num2))
+            elif operacion == '2':
+                print("Resultado: ", calculadora.resta(num1, num2))
+            else:
+                print("Operación no válida. Intente nuevamente.")
